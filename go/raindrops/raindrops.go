d1aa906a94ee414c1e65c73dad9aa3b8b3beac43
Shane McCulley <smcculle@uno.edu>
2017-11-06 18:11:03 -0600

    Formats

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -3,13 +3,13 @@ package raindrops
 import "fmt"
 
 func Convert(i int) (o string) {
-	if i % 3 == 0 {
+	if i%3 == 0 {
 		o += "Pling"
 	}
-	if i % 5 == 0 {
+	if i%5 == 0 {
 		o += "Plang"
 	}
-	if i % 7 == 0 {
+	if i%7 == 0 {
 		o += "Plong"
 	}
 
