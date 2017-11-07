ac9bb4dd336e134b0be3ce12c6ea207f30d7e0c1
Shane McCulley <smcculle@uno.edu>
2017-11-06 18:09:53 -0600

    Replaces toLower with hasLetter and Adds HasSuffix

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -4,13 +4,16 @@ package bob
 
 import (
 	"strings"
+	"unicode"
 )
 
-const testVersion = 3
-const qAns = "Sure."
-const yAns = "Whoa, chill out!"
-const sAns = "Fine. Be that way!"
-const elseAns = "Whatever."
+const (
+	testVersion = 3
+	qAns        = "Sure."
+	yAns        = "Whoa, chill out!"
+	sAns        = "Fine. Be that way!"
+	elseAns     = "Whatever."
+)
 
 // Hey tests a remark to see if it is all capitals (yelling), ends in a question mark (question), or has no text (blank).
 // Returns the constant string associated with each case or a default answer if none match.
@@ -18,7 +21,7 @@ func Hey(remark string) (resp string) {
 
 	switch trimmed := strings.TrimSpace(remark); {
 
-	case isYelling(trimmed):
+	case hasLetter(trimmed) && isYelling(trimmed):
 		resp = yAns
 	case isQuestion(trimmed):
 		resp = qAns
@@ -29,13 +32,24 @@ func Hey(remark string) (resp string) {
 	}
 	return
 }
+
 func isSilence(remark string) bool {
 	return len(remark) == 0
 }
+
 func isQuestion(remark string) bool {
-	return remark != "" && string(remark[len(remark)-1]) == "?"
+	return strings.HasSuffix(remark, "?")
 }
 
 func isYelling(remark string) bool {
-	return strings.ToUpper(remark) == remark && strings.ToLower(remark) != remark
+	return strings.ToUpper(remark) == remark
+}
+
+func hasLetter(s string) bool {
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			return true
+		}
+	}
+	return false
 }
