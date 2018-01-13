4ae916b60222cb5e744c05d510ce9910cdd6a20f
Shane McCulley <smcculle@uno.edu>
2018-01-12 23:44:01 -0600

    Remove map, drop node pointer in array.  6-7x speedup

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -2,7 +2,6 @@ package tree
 
 import (
 	"errors"
-	"sort"
 )
 
 type Record struct {
@@ -14,69 +13,54 @@ type Node struct {
 	Children []*Node
 }
 
-func (n *Node) AddRecord(r Record, processed map[int]*Node) {
-	newNode := &Node{ID: r.ID}
-	n.Children = append(n.Children, newNode)
-	processed[r.ID] = newNode
-}
-
-func SortRecords(records []Record) {
-
-	sort.Slice(records[:], func(i, j int) bool {
-		if records[i].Parent == records[j].Parent {
-			return records[i].ID < records[j].ID
-		}
-		return records[i].Parent < records[j].Parent
-	})
-}
-
 func Build(records []Record) (*Node, error) {
 
-	if len(records) == 0 {
+	n := len(records)
+	if n == 0 {
 		return nil, nil
 	}
 
-	SortRecords(records)
+	tree := make([]Node, len(records))
+	sorted, err := SortRecords(records)
 
-	if records[0].ID != 0 {
-		return nil, errors.New("no root")
-	} else if records[0].Parent != 0 {
-		return nil, errors.New("root has parent")
+	if err != nil {
+		return nil, err
 	}
 
-	rootID := records[0].ID
-	root := &Node{ID: rootID}
-	processed := make(map[int]*Node)
-	processed[rootID] = root
-	current := processed[rootID]
+	for _, r := range sorted[1:] {
+		tree[r.ID].ID = r.ID
+		tree[r.Parent].Children = append(tree[r.Parent].Children, &tree[r.ID])
+	}
 
-	for _, r := range records[1:] {
+	return &tree[0], nil
 
-		err := errChk(records, processed, r)
+}
+
+func SortRecords(records []Record) ([]Record, error) {
+	n := len(records)
+	sorted := make([]Record, n)
+
+	var err error
+	for _, r := range records {
+		err = errChk(r, n)
 		if err != nil {
 			return nil, err
 		}
 
-		if current.ID != r.Parent {
-			current = processed[r.Parent]
-		}
-
-		current.AddRecord(r, processed)
+		sorted[r.ID] = r
 	}
 
-	return root, nil
+	return sorted, nil
 }
 
-func errChk(records []Record, processed map[int]*Node, r Record) error {
+func errChk(r Record, n int) error {
 
-	if processed[r.ID] != nil {
-		return errors.New("duplicate node")
-	} else if r.ID >= len(records) {
+	if r.ID == 0 && r.Parent == 0 {
+		return nil
+	} else if r.Parent >= r.ID {
+		return errors.New("parent ID >= child ID")
+	} else if r.ID >= n {
 		return errors.New("non-continuous")
-	} else if r.Parent > r.ID {
-		return errors.New("parent ID > child ID")
-	} else if processed[r.Parent] == nil {
-		return errors.New("detached tree")
 	} else {
 		return nil
 	}
