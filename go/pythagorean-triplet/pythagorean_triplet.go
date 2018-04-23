3d234986114c408a6f0525482a88b1bb8a350236
Shane McCulley <smcculle@uno.edu>
2018-04-23 15:10:53 -0500

    Add euclid's method, 40x speedup on range and 8000x faster on sum

--- go/pythagorean-triplet/pythagorean_triplet.go
+++ go/pythagorean-triplet/pythagorean_triplet.go
@@ -1,73 +1,173 @@
 package pythagorean
 
-type Factors map[int]int
+import (
+	"math"
+	"sort"
+)
+
 type Triplet [3]int
 
 // Range returns all triplets where each side lies in [min..max]
-func Range(min, max int) []Triplet {
-	var trips []Triplet
+func Range(min, max int) (trips []Triplet) {
 
-	for r := 2; r < max/2; r++ {
-		newTrips := genTriplets(r)
-		trips = append(trips, newTrips...)
-	}
-
-	return filterRange(trips, min, max)
-}
-
-// Sum returns all triplets where x+y+z=p
-func Sum(p int) []Triplet {
-	return filterSum(Range(0, p/2), p)
-}
-
-func filterSum(trips []Triplet, p int) (valid []Triplet) {
-	for _, t := range trips {
-		if t[0]+t[1]+t[2] == p {
-			valid = append(valid, t)
-		}
-	}
-	return
-}
-
-func filterRange(trips []Triplet, min, max int) (valid []Triplet) {
-	for _, t := range trips {
-		if t[0] >= min && t[2] <= max {
-			valid = append(valid, t)
-		}
-	}
-	return
-}
-
-// genTriplets returns a set of pythagorean triplets using Dickson's method
-func genTriplets(r int) []Triplet {
-
-	var trips []Triplet
-
-	if r*r%2 != 0 {
-		return nil
-	}
-
-	for s, t := range getFactors(r * r / 2) {
-		trips = append(trips, convertRST(r, s, t))
-	}
-
-	return trips
-}
-
-func convertRST(r, s, t int) Triplet {
-	x, y, z := r+s, r+t, r+s+t
-	return Triplet{x, y, z}
-}
-
-func getFactors(n int) Factors {
-	factors := Factors{1: n}
-	for i := 2; i < n/2; i++ {
-		if n%i == 0 {
-			f1, f2 := i, n/i
-			if factors[f2] == 0 {
-				factors[f1] = f2
+	for m := minM(min); m <= maxM(max); m++ {
+		// n should start at 1 if m is even or 2 if m is odd and maintain the same even/odd parity
+		for n := 1 + m%2; n < m; n += 2 {
+			if isCoprime(m, n) {
+				a, b, c := euclidPrimitive(m, n)
+				for k := minK(min, a); k <= maxK(max, c); k++ {
+					trips = append(trips, Triplet{k * a, k * b, k * c})
+				}
 			}
 		}
 	}
-	return factors
+	return trips
+}
+
+func validSum(m, s, p int) bool {
+	return p%(m*s) == 0
+}
+
+// Sum returns all triplets where x+y+z=p, solution adapted from
+// https://stackoverflow.com/a/2835053/4304664
+func Sum(p int) []Triplet {
+
+	var trips []Triplet
+
+	if p%2 == 1 {
+		return nil
+	}
+	p /= 2
+
+	limit := sqrt(p)
+	factors := getFactors(p)
+	for _, m := range factors {
+		if m > limit {
+			continue
+		}
+
+		for _, s := range factors {
+			if s >= 2*m || s <= m {
+				continue
+			}
+
+			if validSum(m, s, p) {
+				n := s - m
+				k := p / m / s
+				trips = append(trips, euclidK(k, m, n))
+			}
+		}
+	}
+
+	return dropDuplicates(trips)
+}
+func dropDuplicates(triplets []Triplet) (newTrips []Triplet) {
+	sort.Slice(triplets, func(i, j int) bool { return triplets[i][0] < triplets[j][0] })
+	newTrips = append(newTrips, triplets[0])
+	for i := 1; i < len(triplets); i++ {
+		if triplets[i] != triplets[i-1] {
+			newTrips = append(newTrips, triplets[i])
+		}
+	}
+	return newTrips
+}
+
+// euclidK generates a triplet multiple of a primitive given k
+func euclidK(k, m, n int) Triplet {
+	a, b, c := euclidPrimitive(m, n)
+	return Triplet{k * a, k * b, k * c}
+}
+
+// genEuclid uses Euclid's formula to generate primitives given m > n and exactly one of m,n are even.
+func euclidPrimitive(m, n int) (a, b, c int) {
+	a, b, c = m*m-n*n, 2*m*n, m*m+n*n
+
+	if a > b {
+		a, b = b, a
+	}
+
+	return
+}
+
+func getFactors(n int) (factors []int) {
+
+	for i := 1; i <= sqrt(n); i++ {
+		if n%i == 0 {
+			f1, f2 := i, n/i
+			if f1 == f2 {
+				factors = append(factors, f1)
+			} else {
+				factors = append(factors, f1, f2)
+			}
+		}
+	}
+	return
+}
+
+// GCD uses Dijkstra's algorithm to calculate greatest common divisor
+func GCD(m, n int) int {
+	if m == n {
+		return m
+	} else if m > n {
+		return GCD(m-n, n)
+	}
+
+	return GCD(m, n-m)
+}
+
+func isCoprime(m, n int) bool {
+	return GCD(m, n) == 1
+}
+
+func sqrt(n int) int {
+	return int(math.Sqrt(float64(n)))
+}
+
+func minimum(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
+
+// Functions below deal with satisfying the constraints min <= a < b < c <=max when choosing values for k,m,n
+
+// minK returns the largest value of k meeting constraints.
+// Given the smallest side a, we need k*a >= min  => k >= min/a
+func minK(min, a int) (k int) {
+	if min%a == 0 {
+		k = min / a
+	} else {
+		k = (min / a) + 1
+	}
+
+	return
+}
+
+// maxK returns the largest value of k meeting constraints.
+// Given m,n, we need k*c <= max.  Therefore k <= max/c
+func maxK(max, c int) (k int) {
+	return max / c
+}
+
+// minM returns the largest value of m meeting constraints.
+// Given a triplet T with a < b < c, we need < a < b < c <= max
+// Since c = k(m*m + n*n), the value of m is maximized at k=1 subject to m*m + n*n <= max
+// m > n implies  m*m + n*n <= m*m + m*m <= 2m*m, thus satisfying 2*m*m <= max ensures m*m+n*n <= max.
+// Therefore we need m <= sqrt(max/2)
+func maxM(max int) int {
+	return sqrt(max / 2)
+}
+
+// minM returns the smallest value of m meeting constraints.
+// Given a triplet T with a < b < c, we need min <= a < b < c.
+// a = minimum(2*m*n, m*m - n*n), so we must mimimize both equations. Given m > n, we have
+// 2*m*m >= 2*m*n >= min   ==>  m >= sqrt(min/2)  , and
+//
+// The smallest value of n is 1, so we have
+// m*m - n*n >= m*m - 1*1 >= min  ==>  m >= sqrt(min-1)
+// so minM should be minimum( sqrt(min/2), sqrt(min-1))
+func minM(min int) int {
+	m1, m2 := sqrt(min/2), sqrt(min-1)
+	return minimum(m1, m2)
 }
